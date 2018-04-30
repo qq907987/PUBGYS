73644f074bf66328ffb34b19c611d175b0e84697
annian1314 <38837229+annian1314@users.noreply.github.com>
2018-05-01 05:14:31 +0800

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
 const appData = {
   autoRefresh: true,
-  refreshInterval: Math.floor(1000 / 30),
+  refreshInterval: 1000,
   gsTime: 0,
   me: [-1, -1, 0, 0],
   meGuid: -1,
@@ -37,7 +37,7 @@ vapp = new Vue({
     showItemHealth: false,
     showItemThrow: false,
     showItemAmmo: false,
-    showItemAll: true,
+    showItemAll: false,
 
     // --------------------------------------------------------------------------
 
