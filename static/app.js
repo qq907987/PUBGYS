7f23bf18a3aba29e605cfda095b8dcaa85bfdf0a
annian1314 <38837229+annian1314@users.noreply.github.com>
2018-05-01 02:27:14 +0800

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,6 +1,6 @@
-const appData = {
+﻿const appData = {
   autoRefresh: true,
-  refreshInterval: Math.floor(1000 / 30),
+  refreshInterval: 1000,
   gsTime: 0,
   me: [-1, -1, 0, 0],
   meGuid: -1,
@@ -17,6 +17,7 @@ const appData = {
   itemFeatures: new Map(), // itemguid -> olFeature
 }
 
+// vue app just for html map option control
 vapp = new Vue({
   el: '#app',
   data: {
@@ -25,7 +26,7 @@ vapp = new Vue({
     mapType: 'erangel',
     followMe: true,
     isDesert: false,
-    showBox: true,
+    showBox: false,
     showAirDrop: true,
     showCar: true,
 
@@ -37,7 +38,7 @@ vapp = new Vue({
     showItemHealth: false,
     showItemThrow: false,
     showItemAmmo: false,
-    showItemAll: true,
+    showItemAll: false,
 
     // --------------------------------------------------------------------------
 
@@ -76,7 +77,7 @@ vapp = new Vue({
     // --------------------------------------------------------------------------
 
     coordinate: '',
-    toggleButtonText: ''
+    toggleButtonText: '停止刷新'
   },
   watch: {
     mapType: (val) => {
@@ -92,6 +93,30 @@ vapp = new Vue({
         return 0b11111111111111111111111111111111
       }
       let flags = 0
+      // if (this.showItemTop) {
+      //   flags |= 0b1000000000000000
+      // }
+      // if (this.showItemDuoDuo) {
+      //   flags |= 0b0100000000000000 // 雷 水 疼 急
+      // }
+      // if (this.showItemBasic) {
+      //   flags |= 0b0001010100010000 // 基本出装: 穿戴 | 步枪 | 瞄准 | 狙枪
+      // }
+      // if (this.showItemAR) {
+      //   flags |= 0b0000011000000000 // 步枪和配件
+      // }
+      // if (this.showItemSR) {
+      //   flags |= 0b0000000110000000 // 狙击和配件
+      // }
+      // if (this.showItemHealth) {
+      //   flags |= 0b0000100000000000
+      // }
+      // if (this.showItemThrow) {
+      //   flags |= 0b0000000001000000
+      // }
+      // if (this.showItemAmmo) {
+      //   flags |= 0b0000000000100000
+      // }
       if (this.showBack) {
         flags |= 0b00000000000000000000000000001000
       }
@@ -208,13 +233,17 @@ vapp = new Vue({
 })
 
 const projection = ol.proj.get('EPSG:21781')
+// The extent is used to determine zoom level 0. Recommended values for a
+// projection's validity extent can be found at https://epsg.io/.
+// 0,0 at left bottom, 0, 8192 at left top
 projection.setExtent([0, 0, 8192, 8192])
 
 function getMapSource (mapType) {
   const mapPath = mapType === 'erangel'
     ? 'erangel/v11'
     : 'miramar/v5'
-  let useLocalResource = true
+  // if false, will use https://tiles2-v2.pubgmap.net/tiles/erangel/v11/{z}/{x}/{y}.png not sure if it is stable or not. But it will have more zoom, up to 5. Local only has up to 4
+  let useLocalResource = false
   const mapBase = useLocalResource
     ? '../maptiles'
     : 'https://tiles2-v2.pubgmap.net/tiles'
@@ -377,12 +406,12 @@ const gridSource = new ol.source.Vector({
 })
 const majorLineStyle = new ol.style.Style({
   stroke: new ol.style.Stroke({
-    color: [255, 255, 0, 0.4]
+    color: [255, 255, 0, 0.6]
   })
 })
 const minorLineStyle = new ol.style.Style({
   stroke: new ol.style.Stroke({
-    color: [0xcc, 0xcc, 0xcc, 0.2]
+    color: [0xcc, 0xcc, 0xcc, 0.4]
   })
 })
 const gridLayer = new ol.layer.Vector({
@@ -653,14 +682,14 @@ const renderMap = () => {
       } else if (playerObj.name) {
         label = playerObj.name
       } else {
-        //label = `<${playerObj.guid}>`
+        label = `<${playerObj.name}>`
       }
       if (playerObj.kills) {
-        //label += ` Kill${playerObj.kills}`
+        label += ` |杀:${playerObj.kills}|`
       }
     }
     if (playerObj.health != null) {
-      //label += ` Health${Math.floor(playerObj.health)}`
+      label += ` |血:${Math.floor(playerObj.health)}|`
     }
     feature.set('_label', label)
     // re-add should be fine
@@ -781,7 +810,6 @@ const updatePlayerLocs = () => {
     query += `itemFlags=${vapp.showItemFlags}&`
   }
   axios.get(`/api/gamestate?${query}`).then(res => {
-	  console.log(res.data);
     if (res.data.gsTime !== appData.gsTime) { // means we got a new game start
       console.log('Seems we got a new game', res.data.gsTime, appData.gsTime)
       // refresh browser should be the safest way.
